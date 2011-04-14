08c5c7dd368ee78c3fa0d19bebd3d074bd3820ac
Daniel Wehner <dawehner@googlemail.com>
2011-04-14 17:52:45 +0200

    newton

--- blatt1/main.cc
+++ blatt1/main.cc
@@ -22,7 +22,7 @@ float iteration_easy(float anomalie_excent, float excent, float anomalie_middle)
 float iteration_newton(float anomalie_excent, float excent, float anomalie_middle) {
   float funcvalue = function(anomalie_excent, excent, anomalie_middle);
   float diffvalue = function_diff(anomalie_excent, excent, anomalie_middle);
-  cout << "func:" << funcvalue << endl;
+  std::cout << "func:" << funcvalue << std::endl;
   return anomalie_excent
     - (funcvalue / diffvalue);
 }
