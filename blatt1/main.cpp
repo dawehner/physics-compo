8f4a8fe7750c3020bebcd74b630bed43e0deb3e8
Daniel Wehner <dawehner@googlemail.com>
2011-04-14 15:55:44 +0200

    some changes

--- blatt1/main.cpp
+++ blatt1/main.cpp
@@ -1,7 +1,26 @@
 ﻿#include <cmath>
 
+const ITERATION_EASY = 0;
+const ITERATION_NEWTON = 1;
+
+const ITERATION = ITERATION_EASY;
 int main() {
-  
+  float anomalie_excent, excent, anomalie_middle;
+  while (TRUE) {
+	anomalie_excent = e_next(anomalie_excent, excent, anomalie_middle);
+  }
+}
+
+/**
+ * This code could be OPP.
+ */
+float e_next(float anomalie_excent, float excent, float anomalie_middle) {
+  switch (ITERATION) {
+    case ITERATION_EASY:
+	  return iteration_easy(anomalie_excent, excent, anomalie_middle);
+	case ITERATION_NEWTON:
+	  return iteration_newton(anomalie_excent, excent, anomalie_middle);
+  }
 }
 
 float function(float anomalie_excent, float excent, float anomalie_middle) {
@@ -13,8 +32,12 @@ float function_diff(float excent, float anomalie_excent) {
 }
   
 
-float iteration_easy() {
+float iteration_easy(float anomalie_excent, float excent, float anomalie_middle) {
+  return anomalie_middle + excent * sin(anomalie_excent);
 }
 
 float iteration_newton() {
+  return anomalie_excent
+    - function(anomalie_excent, excent, anomalie_middle)
+    / function_diff(anomalie_excent, excent, anomalie_middle);
 }
