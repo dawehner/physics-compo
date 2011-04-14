2d1c52307744e04b3ef35fa88b705a1379b64a96
Daniel Wehner <dawehner@googlemail.com>
2011-04-14 15:37:09 +0200

    initial version

---
+++ blatt1/main.c
@@ -0,0 +1,18 @@
+﻿int main() {
+  
+}
+
+float function(float anomalie_excent, float excent, float anomalie_middle) {
+  return anomolie - excent * sin(anomolie) - anomalie_middle;
+}
+
+float function_diff(float excent, float anomalie_excent) {
+  return 1.0 - excent * cos(anomalie_excent);
+}
+  
+
+float iteration_easy() {
+}
+
+float iteration_newton() {
+}
