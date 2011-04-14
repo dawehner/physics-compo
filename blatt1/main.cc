e8a9c0bf67d1ea92a3905e028f22a76a450d049b
Daniel Wehner <dawehner@googlemail.com>
2011-04-14 18:38:56 +0200

    sync

--- blatt1/main.cc
+++ blatt1/main.cc
@@ -25,6 +25,7 @@ double iteration_newton(double anomalie_excent, double excent, double anomalie_m
   double result = anomalie_excent
     - (funcvalue / diffvalue);
 	
+  std::cout << "iteration_newton:intput: E:" << anomalie_excent << ":M:" << anomalie_middle << ":e:" << excent << std::endl;
   std::cout << "iteration_newton: f:" << funcvalue << ":f':" << diffvalue << ":E:" << anomalie_excent << std::endl;
   return result;
 }
