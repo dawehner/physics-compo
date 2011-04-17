9718d05c8d737bd2f27005fd5b439861234d746e
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-04-17 23:35:03 +0200

    fix compile errors

--- blatt1/coordinates.cc
+++ blatt1/coordinates.cc
@@ -6,7 +6,7 @@
 double calc_phi(double anomalie_excent, double excent) {
   return 2 * atan(
      sqrt((1+excent)/(1-excent)) *
-	 tan(anomalie_excent / 2));
+    tan(anomalie_excent / 2));
  }
 double calc_r(double a, double e, double phi, double phi0) {
   return a * (1 - e * e) / (1 + e * cos(phi - phi0));
--- blatt1/number2.cc
+++ blatt1/number2.cc
@@ -18,13 +18,35 @@ struct sunsystem_object {
   double m;
   double p;
   string name;
+  double lambda;
 };
 
+
+double distince_sunsystem_objects(sunsystem_object obj1, double anomalie_excent1, sunsystem_object obj2, double anomalie_excent2) {
+  double phi1, phi2;
+  double r1, r2;
+  double x1, x2;
+  double y1, y2;
+
+  phi1 = calc_phi(anomalie_excent1, obj1.e);
+  r1 = calc_r(obj1.a, obj1.e, phi1, obj1.phi0);
+  x1 = calc_x(r1, phi1, obj1.phi0);
+  y1 = calc_y(r1, phi1, obj1.phi0);
+
+  phi2 = calc_phi(anomalie_excent2, obj2.e);
+  r2 = calc_r(obj2.a, obj2.e, phi2, obj2.phi0);
+  x2 = calc_x(r2, phi2, obj2.phi0);
+  y2 = calc_y(r2, phi2, obj2.phi0);
+
+  return (x1- x2)*(x1- x2) + (y1 - y2)*(y1 - y2);
+}
+
 int main() {
-  double t0 = 0;
-  double t = t0;
-  double temp_except;
-  double excent;
+  double t = 0;
+  double distance;
+
+  double earth_temp_except;
+  double mars_temp_except;
 
   sunsystem_object earth;
   earth.name = "Earth";
@@ -37,8 +59,8 @@ int main() {
 
   sunsystem_object mars;
   mars.name = "Mars";
-  mars.a_mars = 1.524;
-  mars.e_mars = 0.0934;
+  mars.a = 1.524;
+  mars.e = 0.0934;
   mars.phi0 = grad_to_rad(336.04);
   mars.lambda = 355.46;
   mars.m = mars.lambda - mars.phi0;
@@ -56,18 +78,10 @@ int main() {
   for (signed int i = (starttime / size) ; i < (endtime / size); i++) {
     t = i * size;
 
-    temp_except = generate_anomalie_excent_per_time(e_earth, t, t0, P);
-    phi = calc_phi(temp_except, excent);
-    r = calc_r(a, excent, phi, phi0);
-    x = calc_x(r, phi, phi0);
-    y = calc_y(r, phi, phi0);
+    mars_temp_except = generate_anomalie_excent_per_time(mars.e, t, t0, mars.p);
+    earth_temp_except = generate_anomalie_excent_per_time(earth.e, t, t0, earth.p);
 
-    cout<<"r" << r;
-    cout<<"phi" << phi << endl;
-
-    // std::cout<<"x" << x;
-    // std::cout<<"y" << y;
-    // plots << x << ";" << y << endl;
+    distance = distince_sunsystem_objects(earth, earth_temp_except, mars, mars_temp_except);
   }
 
   return 0;
