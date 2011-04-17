e85e93008de0b463eb6c97e7afbeae14321efc19
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-04-17 23:12:25 +0200

    add p_mars/earth and remove some unneeded variables

--- blatt1/number2.cc
+++ blatt1/number2.cc
@@ -6,47 +6,43 @@
 
 using namespace std;
 
+const double anstromical_unit = 149598000;
+const int day_seconds = 3600 * 24;
+const int year_seconds = 3600 * 365 * 24;
+
 
 int main() {
   double t0 = 0;
   double t = t0;
-  double temp_except = 0;
-  double excent = 0.205;
-  double P = 10000;
-  double phi = 0;
-  double r = 0;
-  double x = 0;
-  double y = 0;
-  double a = 1000;
-  double phi0 = grad_to_rad(102.95);
-  
-  
+
   double a_earth = 1.0;
   double e_earth = 0.0167;
   double phi0_earth = grad_to_rad(102.95);
   double lambda_earth = 100.46;
   double m_earth = lambda_earth - phi0_earth;
-  
+  double p_earth = year_seconds;
+
   double a_mars = 1.524;
   double e_mars = 0.0934;
   double phi0_mars = grad_to_rad(336.04);
   double lambda_mars = 355.46;
   double m_mars = lambda_mars - phi0_mars;
+  double p_mars = day_seconds * 779.94;
 
   // fstream plots;
   // plots.open("../plots.txt", ios::out);
   // plots.close();
 
-  // 630720000 is 20 years in seconds
   // let's choose not too many steps
   int size = 1000;
-  int max = 630720000 / size;
-  for (int i = 0; i < max; i++) {
-    // We go back in time
-    t -= i * size;
+  int starttime = - 20 * year_seconds;
+  int endtime = 5 * year_seconds;
+  // Let's start from 2000 - 20 years and go until 2000 + 5 year'
+  for (signed int i = (starttime / size) ; i < (endtime / size); i++) {
+    t = i * size;
 
-	  temp_except = generate_anomalie_excent_per_time(excent, t, t0, P);
-	  phi = calc_phi(temp_except, excent);
+    temp_except = generate_anomalie_excent_per_time(e_earth, t, t0, P);
+    phi = calc_phi(temp_except, excent);
     r = calc_r(a, excent, phi, phi0);
     x = calc_x(r, phi, phi0);
     y = calc_y(r, phi, phi0);
