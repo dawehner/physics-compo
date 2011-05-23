5d1046f0c05e163d078d857aea91f579cdaece52
Daniel Wehner <zxmsl13@u-078-c203.uni-tuebingen.de>
2011-05-23 16:30:51 +0200

    Update

--- blatt3/main.cpp
+++ blatt3/main.cpp
@@ -30,7 +30,7 @@ double parabel(double x) {
 }
 
 double gerade(double x) {
-  return 1;
+  return 1.0;
 }
 
 double si(double x) {
@@ -46,6 +46,7 @@ int main() {
   double value = integrate_easy(&si, 0, x, 0.02);
   cout << value << endl;
   value = integrate_trapez(&func_c, 0, x, 0.02);
+  value = integrate_adaptive(&func_c, 0, x, 0.02);
   cout << value << endl;
   return 0;
 }
@@ -98,39 +99,67 @@ double integrate_adaptive (
   double start, double end,
   double step_size) {
 
-
   // Schaetzintegral berechnen.
   double int_schatz = integrate_trapez(function, start, end, step_size);
   // Maschinengenauigkeit beachten.
-  double delta = e-20;
-  double epsilon = e-8;
+  double delta = pow(10, -17);
+  double epsilon = pow(10, -8);
   int_schatz =  int_schatz * epsilon / delta;
 
-  vector <double> ai;
-  vector <double> fi;
-  vector <double> ui;
 
-  // Startwerte setzen
-  ai[0] = start;
-  ai[1] = end;
-  fi[0] = function(start);
-  fi[1] = function(end);
-  double sum = 0.0;
-  
-  double j = 0;
-  double k = 1;
-  double p = 1;
-  double l = 1;
-  double ui[1] = 1;
-  double fm = 0.0;
+  vector<double> func_values;
+  vector<double> func_pos_a;
 
+
+  // Startwerte setzen:
+  func_values[0] = function(start);
+  func_values[1] = function(end);
+  func_pos_a[0] = start;
+  func_pos_a[1] = end;
+
+  double int_sum = 0.0;
+  int grenze_links_j = 0;
+  int grenze_rechts_k = 1;
+  int p = 1;
+  int tiefe_l;
+
+  vector<double> u;
+  u[1] = 1;
+
+  double h = 0.0;
   double m = 0.0;
+  double fm = 0.0;
+  double int_1 = 0.0;
+  double int_2 = 0.0;
 
-  while (l > 0) {
-    h = ai[k] - ai[j];
-    
-    m = (ai[j] + ai[k]) / 2;
+  do {
+    cout << 123 << endl;
+    h = func_pos_a[grenze_rechts_k] - func_pos_a[grenze_links_j];
+    m = (func_pos_a[grenze_rechts_k] + func_pos_a[grenze_links_j]) / 2;
+
     fm = function(m);
-  }    
+    int_1 = h*(func_values[grenze_links_j] + func_values[grenze_rechts_k]) / 2;
+    int_2 = (int_1 + 2 * h * fm) / 3;
+
+    // Genauigkeit beachten
+    if ((int_schatz + int_1 != int_schatz + int_2)) {
+      p++;
+      func_pos_a[p] = m;
+      func_values[p] = fm;
+      grenze_rechts_k = p;
+      tiefe_l++;
+      cout << 123 << endl;
+      u[tiefe_l] = p;
+    }
+    else {
+      int_sum += int_2;
+      grenze_links_j = u[tiefe_l];
+      tiefe_l --;
+      grenze_rechts_k = u[tiefe_l];
+    }
+  }
+  while (tiefe_l > 0);
+
+  return int_sum;
 }
 
---
+++ blatt3/test.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  vector<double> test;
+
+  test[5] = 3.0;
+  return 0;
+}
