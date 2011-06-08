ae083daa400fbd4f6a583469fc025fc5dfe1fb8a
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-06-08 09:48:03 +0200

    update

--- blatt4/blatt4.py
+++ blatt4/blatt4.py
@@ -1,28 +1,31 @@
 from math import sqrt
+
+# Calculate the distance between a two-dim vector.
 def norm(x, y):
   return sqrt(x*x + y*y)
 
-data_newton = "newton/output-newton.dat"
-data_leap = "leap/output-leap.dat"
 
-f_newton = open(data_newton, "r")
-f_leap = open(data_leap, "r")
+def files_substract_values(filename1, filename2, start = 0):
+  file1 = open(filename1, "r")
+  file2 = open(filename2, "r")
 
-lines_newton = []
-for line in f_newton:
-  lines_newton.append(line)
+  # First load all ines
+  lines1 = []
+  for line in file1:
+    lines1.append(line)
 
-lines_leap = []
-for line in f_leap:
-  lines_leap.append(line)
+  lines2 = []
+  for line in file2:
+    lines2.append(line)
 
-print len(lines_newton)
-print len(lines_leap)
+  i = 0
 
-i = 0
+  # Substract the values of r
+  for ln in lines1:
+    line1 = ln.replace("e", "E").rsplit("\t")
+    line2 = lines2[i].replace("e", "E").rsplit("\t")
+    print norm(float(line1[start]), float(line1[start+1])) - norm(float(line2[start]), float(line2[start+1]))
+    i = i + 1
 
-for ln in lines_newton:
-  line_newton = ln.replace("e", "E").rsplit("\t")
-  line_leap = lines_leap[i].replace("e", "E").rsplit("\t")
-  print norm(float(line_newton[0]), float(line_newton[1])) - norm(float(line_leap[0]), float(line_leap[1]))
-  i = i + 1
+
+files_substract_values("leap/output-leap.dat", "newton/output-newton.dat")
