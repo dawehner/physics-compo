273e1584444fde4acc90bcdb00b28cf19035e7bc
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-06-11 10:57:21 +0200

    implement rk4 and euler

--- integrator/integration.cpp
+++ integrator/integration.cpp
@@ -10,10 +10,38 @@ void integration_rk4(listDouble& y, listDouble& dydx, const double x, const doub
   int size = y.size();
 
   listDouble K1(size), K2(size), K3(size), K4(size);
+  listDouble y_temp(size);
 
+  K1 = dydx;
+
+  const double h_2 = h/2;
+  for (int i = 0; i < size; i++) {
+    y_temp[i] = y[i] + h_2 * K1[i];
+  }
+  derivative(x, y_temp, K2);
+
+  for (int i = 0; i < size; i++) {
+    y_temp[i] = y[i] + h_2 * K2[i];
+  }
+  derivative(x, y_temp, K3);
+
+  for (int i = 0; i < size; i++) {
+    y_temp[i] = y[i] + h * K3[i];
+  }
+  derivative(x, y_temp, K4);
+
+  // @todo: Find out whether the compiler automatically optimize it.
+  const double h_6 = h/6;
+  for (int i = 0; i < size; i++) {
+    y_out[i] = y[i] + h_6 * (K1[i] + 2 * K2[i] + 2 * K3[i] + K4[i]);
+  }
 }
 void integration_euler(listDouble& y, listDouble& dydx, const double x, const double h, listDouble& y_out,
-  void derivative(const double x, listDouble& y, listDouble& dyxy_out)
-);
+  void derivative(const double x, listDouble& y, listDouble& dyxy_out)) {
+
+  for (int i = 0; i < y.size(); i++) {
+    y_out[i] = y[i] + dydx[i];
+  }
+}
 
 #endif
