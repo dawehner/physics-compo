25834fc2f5089cd73b72e338add7d09e6648417a
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-07-07 11:01:10 +0200

    Revert "remove old code"
    This reverts commit 586807e3f4867f633360b9f4036547fb54901194.

--- hsolar/hsolar.cpp
+++ hsolar/hsolar.cpp
@@ -59,6 +59,11 @@ void hsolar_solve(double t1, double dt, double n) {
 
     hsolar_write(file_rho, rho);
     hsolar_write(file_u, u);
+//     cout << "#################################################" << endl;
+
+//     for (int i = 0; i < rho.size(); i++) {
+//       cout << rho[i] << endl;
+//     }
 
     t += dt;
   }
