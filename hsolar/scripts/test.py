d9cfabbb8ceff5bebd4a48c7d9c75e908330b780
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-07-07 12:58:50 +0200

    remove pngs at the end

--- hsolar/scripts/test.py
+++ hsolar/scripts/test.py
@@ -3,3 +3,4 @@ import os;
 os.system("../build/hsolar -e 50.0")
 os.system("octave plot_rho.m")
 os.system("mencoder mf://\*.png -mf fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o output.avi -ffourcc DX50")
+os.system("rm *.png")
