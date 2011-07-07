e75c47ed74dbc88b98d67b4fa9ae6702f653c00f
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-07-07 11:07:23 +0200

    add parameters via command

--- hsolar/main.cpp
+++ hsolar/main.cpp
@@ -1,6 +1,25 @@
 #include "hsolar.cpp"
 
 int main(int argc, char **argv) {
-  hsolar_solve(100.0, 0.001, 1.5);
+  double time_end = 100.0;
+  double time_step = 0.001;
+  double poly_n = 1.5;
+
+  int c;
+  while ((c = getopt(argc, argv, ":n:e:t:")) != -1) {
+    switch (c) {
+      case 'e':
+        time_end = atof(optarg);
+        break;
+      case 't':
+        time_step = atof(optarg);
+        break;
+      case 'n':
+        poly_n = atof(optarg);
+        break;
+    }
+  }
+
+  hsolar_solve(time_end, time_step, 1.5);
   return 0;
 }
