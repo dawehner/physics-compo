a04bf61a3c85a209384ef4bcc4741b24c81d542a
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-07-07 12:56:13 +0200

    add a way to change some parameters much easier

--- hsolar/scripts/test.py
+++ hsolar/scripts/test.py
@@ -1,5 +1,5 @@
 import os;
 
-call = "../build/hsolar && octave plot_rho.m && mencoder mf://\*.png -mf fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o output.avi -ffourcc DX50"
-
-os.system(call)
+os.system("../build/hsolar -e 50.0")
+os.system("octave plot_rho.m")
+os.system("mencoder mf://\*.png -mf fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o output.avi -ffourcc DX50")
