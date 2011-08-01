e728540049c937b6da56e8b22faa202b5cb70d46
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-08-01 10:15:07 +0200

    use 2body input file in the scripts

--- nbody/build/nbody.py
+++ nbody/build/nbody.py
@@ -3,7 +3,7 @@ import os;
 
 def nbody_output_helper(name, method):
   # Generate the output, create a directory for it and move all files into it.
-  os.system("./nbody -o output-%s -i %d -v %s" % (name, method, name))
+  os.system("./nbody -o output-%s -i %d -f %s" % (name, method, name))
   os.mkdir(str(name) + "-result")
   os.system("mv output-%s* %s-result/" % (name, name))
   os.chdir(str(name) + "-result")
