c46cb1f1f7e913459522ebbe304a12051375962d
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-08-10 16:36:26 +0200

    quite some updates

--- nbody/build/nbody-3.py
+++ nbody/build/nbody-3.py
@@ -22,7 +22,7 @@ def nbody_find_stable(values, delta_start, delta_end, steps = 100, brk = False, 
     if (result):
       print "Unstable at delta:${0} prefix:{1}".format(delta, prefix)
       if brk:
-        return False
+        return {'delta': delta, 'prefix': prefix}
 
   if (output):
     subprocess.call(['rm', collection_folder, '-Rf'])
@@ -41,10 +41,15 @@ values_orig = [[1.0, 0.0, 0.0], [0.00001, 0.0, 1.0, 1.0], [0.00001, 0.0, 1.0, -1
 
 # Aufgabe 3.3
 
-for i in range(1, 20):
+subprocess.call(['mkdir', 'nbody-3c', '-p'])
+file_delta_crit = open("nbody-3c/delta_crit.dat", 'w')
+
+for i in range(1, 10):
   power = -5.0 - i / 2.0
   m3 = pow(10.0, power)
   print "m3:{}".format(power)
   values_orig[2][0] = m3
-  nbody_find_stable(values_orig, 0.07, 0.00, 100, True, 'nbody-3c', "m3-{}".format(power), False)
+  result = nbody_find_stable(values_orig, 0.07, 0.00, 100, True, 'nbody-3c', "m3-{}".format(power), False)
+  file_delta_crit.write("{} {}".format(pow(10.0, power), result['delta']))
 
+file_delta_crit.close()
