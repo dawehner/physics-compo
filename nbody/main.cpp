4e54a614a96c977fd76e8b6263331ed6730bd997
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-08-16 09:52:15 +0200

    add some comments

--- nbody/main.cpp
+++ nbody/main.cpp
@@ -246,6 +246,9 @@ void output_converseved_quantities(std::ofstream& output_file_conserved,
   }
 }
 
+/**
+ * Adapt the current timestamp dt based on the a and da.
+ */
 void nbody_adapt_timestamp(const double& dt_begin, double& dt, listv3d& a, listv3d& da) {
   double min = numeric_limits<double>::max();
   double val = 0.0;
@@ -258,6 +261,10 @@ void nbody_adapt_timestamp(const double& dt_begin, double& dt, listv3d& a, listv
   dt = dt_begin * min;
 }
 
+/**
+ * Load the initial positions/speeds/accells/daccells/masses
+ * out of the filename.
+ */
 void nbody_load_from_file(listv3d& r, listv3d& v, listv3d& a, listv3d& da, listdouble& m, string& filename) {
   ifstream file(filename.c_str());
   string input_str;
@@ -307,6 +314,9 @@ void nbody_load_from_file(listv3d& r, listv3d& v, listv3d& a, listv3d& da, listd
   }
 }
 
+/**
+ * Calculate the influence radius using eq(39)
+ */
 void nbody_calc_influence_radius(listdouble& R_in, const listdouble& m, const listv3d& r) {
   int central_body = 0;
 
@@ -318,6 +328,9 @@ void nbody_calc_influence_radius(listdouble& R_in, const listdouble& m, const li
   }
 }
 
+/**
+ * Calculate the mass center and change the positions relative to this mass center.
+ */
 void nbody_prepare_mass_center_system(listv3d& r, listv3d& v, listdouble m) {
   listv3d r_ = r;
   listv3d v_ = v;
@@ -353,7 +366,11 @@ void nbody_prepare_mass_center_system(listv3d& r, listv3d& v, listdouble m) {
   }
 }
 
-
+/**
+ * look up whether some bodies are near enough to have a closed encounter.
+ *
+ * If it's near enough count up count_encounter.
+ */
 bool nbody_detect_closed_encounter(int& count_encounter, listdouble& m, listdouble& R_in, listv3d& r, const double ti) {
   bool ret = false;
   int size = R_in.size();
